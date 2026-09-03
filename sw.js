b6157e70a1021f6268aa1028feb54ff736012c5e
KRCMOLD-ARGE <arge@krcmold.com>
2026-09-03 16:08:49 +0300

    Refresh PWA cache after portion modal fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kraw-yemek-v4';
+const CACHE='kraw-yemek-v5';
 const SHELL=['./','./index.html','./app.html','./manifest.webmanifest','./icon.svg','./kraw-wallpaper.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
