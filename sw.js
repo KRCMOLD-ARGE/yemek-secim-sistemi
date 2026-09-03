875de36e9dff2b525c288f2c4ae0c58b683ed4db
KRCMOLD-ARGE <arge@krcmold.com>
2026-09-03 14:47:27 +0300

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE='kraw-yemek-v1';
+const SHELL=['./','./index.html','./app.html','./manifest.webmanifest','./icon.svg','./kraw-wallpaper.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  const url=new URL(req.url);
+  if(req.method!=='GET') return;
+  if(url.hostname.includes('supabase.co')) return;
+
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+        return res;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached=>{
+      const network=fetch(req).then(res=>{
+        if(res && res.ok){
+          const copy=res.clone();
+          caches.open(CACHE).then(cache=>cache.put(req,copy));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached || network;
+    })
+  );
+});
